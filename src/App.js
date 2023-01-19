02e46224a361e4345c45a3231c04f66243ddea3a
Lyssa <lyssa.rodrigues@upe.br>
2023-01-19 09:15:03 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import GlobalStyle from "./styles/global";
+import styled from "styled-components";
+import Form from "./components/Form.js";
+import Grid from "./components/Grid";
+import { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify"; //é o feedback que da em tela quando o usuario faz alguma acao
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+
+
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2``;
+
+
+function App() {
+  
+  const [produtos, setProdutos] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
+
+  const getProdutos = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800"); 
+      setProdutos(res.data.sort((a, b) => (a.descricao> b.descricao ? 1 : -1)));
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getProdutos();
+  }, [setProdutos]);
+
+  return (
+    <>
+
+    <Container>  
+      <Title>FLUXO DE CAIXA</Title>
+      <Form onEdit={onEdit} setOnEdit={setOnEdit} getProdutos={getProdutos} />
+        <Grid setOnEdit={setOnEdit} produtos={produtos} setProdutos={setProdutos} />
+    </Container>
+    <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+    <GlobalStyle />
+    
+    </>
+
+
+  );
+  }
+
+export default App
